0e24128caab14d1cfcc46bb4bf873a1c29864342
gl4zis <maker64@inbox.ru>
2025-11-04 23:38:56 +0300

    project start

---
+++ src/test/java/ru/itmo/infsec/InfSecExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itmo.infsec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfSecExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
